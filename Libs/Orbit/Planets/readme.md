a014ea27ee8f01910d8d915bbaea9b0fc4d9e49a
medaka <36759068+medaka0213@users.noreply.github.com>
2024-02-11 19:44:19 +0900

    fix: 地球軌道の星空修正

---
+++ Libs/Orbit/Planets/readme.md
@@ -0,0 +1,14 @@
+# テクスチャフリー素材一覧
+
+### Milky Way
+
+ヨーロッパ南天文台 (ESO) の天の川のパノラマ画像
+
+https://www.eso.org/public/images/eso0932a/
+
+### Solar Textures
+
+
+惑星のテクスチャ
+
+https://www.solarsystemscope.com/textures/
--- Libs/Orbit/Script/OrbitSimu/OrbitSimu.cs
+++ Libs/Orbit/Script/OrbitSimu/OrbitSimu.cs
@@ -15,7 +15,7 @@ public class OrbitSimu : UdonSharpBehaviour
     public float longitude = 0f;
     public float height = 420f;
     public float direction = 0f;
-    
+
     public float radiusOfEarth = 6371f;
     public float radiusOfCelestialSphere = 10000f;
 
@@ -24,28 +24,29 @@ public class OrbitSimu : UdonSharpBehaviour
     private Transform EarthPosition;
     private Transform SunPosition;
 
-    private Transform  skyboxLatitude;
-    private Transform  skyboxLongitude;
-    private Transform  skyboxTimeOffset;
-    private Transform  skyboxDayOffset;
-    private Transform  skyboxAir;
+    private Transform skyboxLatitude;
+    private Transform skyboxLongitude;
+    private Transform skyboxTimeOffset;
+    private Transform skyboxDayOffset;
+    private Transform skyboxAir;
 
-    private Transform  earthLatitude;
-    private Transform  earthLongitude;
+    private Transform earthLatitude;
+    private Transform earthLongitude;
 
-    private Transform  sunLatitude;
-    private Transform  sunLongitude;
-    private Transform  sunTimeOffset;
-    private Transform  sunDayOffset;
+    private Transform sunLatitude;
+    private Transform sunLongitude;
+    private Transform sunTimeOffset;
+    private Transform sunDayOffset;
 
     public float earthCenterPosY = 5000f;
-    
+
 
     void Start()
     {
         skyboxLatitude = gameObject.transform.Find("SkyBox/Latitude");
         skyboxLongitude = gameObject.transform.Find("SkyBox/Latitude/Longitude");
         skyboxTimeOffset = gameObject.transform.Find("SkyBox/Latitude/Longitude/Time-Offset");
+        skyboxDayOffset = gameObject.transform.Find("SkyBox/Latitude/Longitude/Time-Offset/Axis/Season-Offset");
         skyboxAir = gameObject.transform.Find("SkyboxAir");
 
         EarthPosition = gameObject.transform.Find("Earth");
@@ -60,22 +61,26 @@ public class OrbitSimu : UdonSharpBehaviour
         sunDayOffset = gameObject.transform.Find("Sun/Latitude/Longitude/Time-Offset/Axis/Season-Offset");
     }
 
-    void Update() {
+    void Update()
+    {
         UpdateTransForm();
         UpdateTimeOffset();
         UpdateHorizon();
-        if (orbit){
+        if (orbit)
+        {
             UpdateOrbit();
         }
-        if (atmosphereSimu){
+        if (atmosphereSimu)
+        {
             UpdateAtm();
         }
     }
 
-    public void UpdateTransForm () {
+    public void UpdateTransForm()
+    {
         float scaleOfEarth = earthCenterPosY * (radiusOfEarth / (radiusOfEarth + height));
 
-        transform.localRotation = Quaternion.Euler(0f, - direction, 0f);
+        transform.localRotation = Quaternion.Euler(0f, -direction, 0f);
 
         skyboxLatitude.localRotation = Quaternion.Euler(90f - latitude, 0f, 0f);
         earthLatitude.localRotation = Quaternion.Euler(90f - latitude, 0f, 0f);
@@ -89,47 +94,52 @@ public class OrbitSimu : UdonSharpBehaviour
         sunTimeOffset.localRotation = Quaternion.Euler(0f, (timeInDay - dayInYear - 0.5f) * 360f, 0f);
 
         sunDayOffset.localRotation = Quaternion.Euler(0f, dayInYear * 360f, 0f);
-        
-        EarthPosition.localPosition = new Vector3(0f, - earthCenterPosY, 0f);
-        SunPosition.localPosition = new Vector3(0f, - earthCenterPosY, 0f);
+        skyboxDayOffset.localRotation = Quaternion.Euler(0f, 0f, -dayInYear * 360f);
 
-        EarthPosition.localScale  = new Vector3(
-            scaleOfEarth, 
-            scaleOfEarth, 
+        EarthPosition.localPosition = new Vector3(0f, -earthCenterPosY, 0f);
+        SunPosition.localPosition = new Vector3(0f, -earthCenterPosY, 0f);
+
+        EarthPosition.localScale = new Vector3(
+            scaleOfEarth,
+            scaleOfEarth,
             scaleOfEarth
         );
     }
 
-    public void UpdateTimeOffset(){
-        if (!isDebug){
+    public void UpdateTimeOffset()
+    {
+        if (!isDebug)
+        {
             var now = DateTime.UtcNow;
             var timeSpan = new TimeSpan(now.Hour, now.Minute, now.Second);
             var daySpan = now - new DateTime(now.Year, 1, 1);
-            
+
             timeInDay = (float)timeSpan.TotalSeconds / (24f * 60f * 60f);
             dayInYear = (float)daySpan.TotalDays / 365f;
         }
     }
 
-    public void UpdateHorizon(){
+    public void UpdateHorizon()
+    {
         float angleHorizon = (float)Math.Acos(radiusOfEarth / (radiusOfEarth + height));
-        float skyboxAirHeight = /*radiusOfEarth + height*/ - (float)Math.Tan(angleHorizon) * radiusOfCelestialSphere;
-        skyboxAir.localPosition = new Vector3(0f, skyboxAirHeight, 0f );
+        float skyboxAirHeight = /*radiusOfEarth + height*/ -(float)Math.Tan(angleHorizon) * radiusOfCelestialSphere;
+        skyboxAir.localPosition = new Vector3(0f, skyboxAirHeight, 0f);
     }
 
-    public  void UpdateOrbit()
+    public void UpdateOrbit()
     {
         latitude = orbit.latitude;
         longitude = orbit.longitude;
         height = orbit.altitude;
         direction = orbit.azimuth;
     }
-    public  void UpdateAtm()
+    public void UpdateAtm()
     {
         atmosphereSimu.altitude = height;
     }
 
-    public void SetHeight(float height){
+    public void SetHeight(float height)
+    {
         this.height = height;
     }
 }
